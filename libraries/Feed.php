08ac5776e35b1274379478edb991d9f65ead541e
Roumen Damianoff <roumen@dawebs.com>
2015-12-07 19:19:48 +0200

    updated Feed class (issue #1)

--- libraries/Feed.php
+++ libraries/Feed.php
@@ -3,7 +3,7 @@
  * Feed generator class for ci-feed library.
  *
  * @author Roumen Damianoff <roumen@dawebs.com>
- * @version 1.2.2
+ * @version 1.3.1
  * @link http://roumen.it/projects/ci-feed
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
@@ -11,12 +11,21 @@
 class Feed
 {
 
-    public $items = array();
+    protected $items = array();
     public $title = 'My feed title';
     public $description = 'My feed description';
     public $link;
+    public $logo;
+    public $icon;
     public $pubdate;
     public $lang;
+    public $charset = 'utf-8';
+    public $ctype = null;
+    protected $shortening = false;
+    protected $shorteningLimit = 150;
+    protected $dateFormat = 'datetime';
+    protected $namespaces = array();
+    protected $customView = null;
 
 
     /**
@@ -27,18 +36,51 @@ class Feed
      * @param string $link
      * @param string $pubdate
      * @param string $description
+     * @param string $content
+     * @param array $enclosure (optional)
      *
      * @return void
      */
-    public function add($title, $author, $link, $pubdate, $description)
+    public function add($title, $author, $link, $pubdate, $description, $content='', $enclosure = array())
     {
+
+        if ($this->shortening)
+        {
+            $description = mb_substr($description, 0, $this->shorteningLimit, 'UTF-8');
+        }
+
+        $pubdate = $this->formatDate($pubdate);
+
         $this->items[] = array(
             'title' => $title,
             'author' => $author,
             'link' => $link,
             'pubdate' => $pubdate,
-            'description' => $description
+            'description' => $description,
+            'content' => $content,
+            'enclosure' => $enclosure
         );
+    }
+
+
+    /**
+     * Add new item to $items array
+     *
+     * @param array $a
+     *
+     * @return void
+     */
+    public function addArray(array $a)
+    {
+
+        if ($this->shortening)
+        {
+            $a['description'] = mb_substr($a['description'], 0, $this->shorteningLimit, 'UTF-8');
+        }
+
+        $a['pubdate'] = $this->formatDate($a['pubdate']);
+
+        $this->items[] = $a;
     }
 
 
@@ -46,28 +88,235 @@ class Feed
      * Returns aggregated feed with all items from $items array
      *
      * @param string $format (options: 'atom', 'rss')
+     * @param carbon|datetime|integer $cache (0 - turns off the cache)
+     * @param string $key
      *
      * @return view
      */
-    public function render($format = 'atom')
+    public function render($format = null, $cache = null, $key = null)
     {
+
         $CI =& get_instance();
+
+        if ($format == null && $this->customView == null) $format = "atom";
+        if ($this->customView == null) $this->customView = $format;
+        if ($cache != null) $this->caching = $cache;
+        if ($key != null) $this->cacheKey = $key;
+
+        if ($this->ctype == null)
+        {
+            switch ($format)
+            {
+                case "rss":
+                    $this->ctype = 'application/rss+xml';
+                    break;
+                case "atom":
+                    $this->ctype = 'application/atom+xml';
+                    break;
+                default:
+                    $this->ctype = 'application/atom+xml';
+                    break;
+            }
+        }
 
         if (empty($this->lang)) $this->lang = $CI->config->item('language');
         if (empty($this->link)) $this->link = $CI->config->item('base_url');
         if (empty($this->pubdate)) $this->pubdate = date('D, d M Y H:i:s O');
 
-        $data['channel'] = array(
+        $this->pubdate = $this->formatDate($this->pubdate, $format);
+
+        $channel = array(
             'title'=>$this->title,
             'description'=>$this->description,
+            'logo' => $this->logo,
+            'icon' => $this->icon,
             'link'=>$this->link,
-            'lang'=>$this->lang,
-            'pubdate'=>$this->pubdate
+            'pubdate'=>$this->pubdate,
+            'lang'=>$this->lang
         );
 
-        $data['items'] = $this->items;
+        if ($format == 'rss')
+        {
+            $channel['title'] = html_entity_decode(strip_tags($channel['title']));
+            $channel['description'] = html_entity_decode(strip_tags($channel['description']));
 
-        $CI->load->view('feed/'.$format, $data);
+            foreach($this->items as $k => $v)
+            {
+                // escaping & in description, based on http://stackoverflow.com/questions/1328538/how-do-i-escape-ampersands-in-xml
+                $this->items[$k]['description'] = str_replace('&', '&amp;amp;', html_entity_decode(strip_tags($this->items[$k]['description'])));
+                $this->items[$k]['title'] = html_entity_decode(strip_tags($this->items[$k]['title']));
+            }
+        }
+
+        $viewData = array(
+            'items'         => $this->items,
+            'channel'       => $channel,
+            'namespaces'    => $this->getNamespaces(),
+            'ctype'         => $this->ctype,
+            'charset'       => $this->charset
+        );
+
+        $CI->load->view('feed/'.$this->customView, $viewData);
+
+     }
+
+
+    /**
+      * Create link
+      *
+      * @param string $url
+      * @param string $format
+      *
+      * @return string
+      */
+     public function link($url, $format='atom')
+     {
+
+        if ($this->ctype == null)
+        {
+            switch ($format)
+            {
+                case "rss":
+                    $type = 'application/rss+xml';
+                    break;
+                case "atom":
+                    $type = 'application/atom+xml';
+                    break;
+                default:
+                    $type = 'application/atom+xml';
+                    break;
+            }
+        }
+        else
+        {
+            $type = $this->ctype;
+        }
+
+        return '<link rel="alternate" type="'.$type.'" href="'.$url.'" />';
+     }
+
+
+    /**
+     * Set Custom view if you don't like the ones that come built in with the package
+     *
+     * @param string $name
+     *
+     * @return void
+     */
+    public function setView($name=null)
+    {
+        $this->customView = $name;
     }
+
+
+    /**
+     * Set maximum characters lenght for text shortening
+     *
+     * @param integer $l
+     *
+     * @return void
+     */
+    public function setTextLimit($l=150)
+    {
+        $this->shorteningLimit = $l;
+    }
+
+
+    /**
+     * Turn on/off text shortening for item content
+     *
+     * @param boolean $b
+     *
+     * @return void
+     */
+    public function setShortening($b=false)
+    {
+        $this->shortening = $b;
+    }
+
+
+    /**
+     * Format datetime string, timestamp integer or carbon object in valid feed format
+     *
+     * @param string/integer $date
+     *
+     * @return string
+     */
+    protected function formatDate($date, $format="atom")
+    {
+        if ($format == "atom")
+        {
+            switch ($this->dateFormat)
+            {
+                case "carbon":
+                    $date = date('c', strtotime($date->toDateTimeString()));
+                    break;
+                case "timestamp":
+                    $date = date('c', $date);
+                    break;
+                case "datetime":
+                    $date = date('c', strtotime($date));
+                    break;
+            }
+        }
+        else
+        {
+            switch ($this->dateFormat)
+            {
+                case "carbon":
+                    $date = date('D, d M Y H:i:s O', strtotime($date->toDateTimeString()));
+                    break;
+                case "timestamp":
+                    $date = date('D, d M Y H:i:s O', $date);
+                    break;
+                case "datetime":
+                    $date = date('D, d M Y H:i:s O', strtotime($date));
+                    break;
+            }
+        }
+
+
+        return $date;
+    }
+
+
+    /**
+     * Add namespace
+     *
+     * @param string $n
+     *
+     * @return void
+     */
+    public function addNamespace($n)
+    {
+        $this->namespaces[] = $n;
+    }
+
+
+    /**
+     * Get all namespaces
+     *
+     * @param string $n
+     *
+     * @return void
+     */
+    public function getNamespaces()
+    {
+        return $this->namespaces;
+    }
+
+
+    /**
+     * Setter for dateFormat
+     *
+     * @param string $format
+     *
+     * @return void
+     */
+    public function setDateFormat($format="datetime")
+    {
+        $this->dateFormat = $format;
+    }
+
 
 }
