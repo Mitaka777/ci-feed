22abca932d93cf8f6792c3bcce3b08d43ef0da61
Roumen Damianoff <roumen@dawebs.com>
2015-12-07 19:18:55 +0200

    updated views

--- views/feed/atom.php
+++ views/feed/atom.php
@@ -1,22 +1,30 @@
-<?php header('Content-type: application/atom+xml; charset=utf-8'); ?>
-<?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
-<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
-    <channel>
-        <title><![CDATA[<?php echo $channel['title'] ?>]]></title>
-        <link><?php echo $channel['link'] ?></link>
-        <description><![CDATA[<?php echo $channel['description'] ?>]]></description>
-        <atom:link href="<?php echo $channel['link'] ?>" rel="self"></atom:link>
-        <language><?php echo $channel['lang'] ?></language>
-        <lastBuildDate><?php echo date('D, d M Y H:i:s O', strtotime($channel['pubdate'])) ?></lastBuildDate>
-        <?php foreach($items as $item): ?>
-        <item>
-            <title><![CDATA[<?php echo $item['title'] ?>]]></title>
-            <link><?php echo $item['link'] ?></link>
-            <guid isPermaLink="true"><?php echo $item['link'] ?></guid>
-            <description><![CDATA[<?php echo $item['description'] ?>]]></description>
-            <dc:creator xmlns:dc="http://purl.org/dc/elements/1.1/"><?php echo $item['author'] ?></dc:creator>
-            <pubDate><?php echo date('D, d M Y H:i:s O', strtotime($item['pubdate'])) ?></pubDate>
-        </item>
+<?php header('Content-type: '.$ctype.'; charset='.$charset); ?>
+<?php echo '<?xml version="1.0" encoding="utf-8"?>'."\n"; ?>
+<feed xmlns="http://www.w3.org/2005/Atom"<?php foreach($namespaces as $n) echo " ".$n; ?>>
+    <title type="html"><?php echo $channel['title'] ?></title>
+    <subtitle type="html"><?php echo $channel['description'] ?></subtitle>
+    <link href="<?php echo $channel['link'] ?>"></link>
+    <id><?php echo $channel['link'] ?></id>
+    <link rel="alternate" type="text/html" href="<?php echo $channel['link'] ?>" ></link>
+    <link rel="self" type="application/atom+xml" href="<?php echo $channel['link'] ?>" ></link>
+    <?php if (!empty($channel['logo'])) : ?>
+    <logo><?php echo $channel['logo'] ?></logo>
+    <?php endif; ?>
+    <?php if (!empty($channel['icon'])) : ?>
+    <icon><?php echo $channel['icon'] ?></icon>
+    <?php endif; ?>
+        <updated><?php echo $channel['pubdate'] ?></updated>
+        <?php foreach($items as $item) : ?>
+        <entry>
+            <author>
+                <name><![CDATA[<?php echo $item['author'] ?>]]></name>
+            </author>
+            <title type="html"><![CDATA[<?php echo $item['title'] ?>]]></title>
+            <link rel="alternate" type="text/html" href="<?php echo $item['link'] ?>"></link>
+            <id><?php echo $item['link'] ?></id>
+            <summary type="html"><![CDATA[<?php echo $item['description'] ?>]]></summary>
+            <content type="html"><![CDATA[<?php echo $item['content'] ?>]]></content>
+            <updated><?php echo $item['pubdate'] ?></updated>
+        </entry>
         <?php endforeach; ?>
-    </channel>
-</rss>
+</feed>
